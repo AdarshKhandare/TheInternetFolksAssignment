1c798085bd0f1fc4623a44386b805ff254af9763
Adarsh M. Khandare <kingadarsh313@gmail.com>
2022-07-13 20:15:00 +0530

    half done

--- theinternetfolksassignment/components/UrlShortner/UrlShortner.jsx
+++ theinternetfolksassignment/components/UrlShortner/UrlShortner.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import {
   UrlShortnerButton,
   UrlShortnerContainer,
@@ -12,11 +13,46 @@ import {
 } from "../../styles/UrlShortner/UrlShortner.element";
 
 const UrlShortner = () => {
+  const [longUrl, setLongUrl] = React.useState("");
+  const onCreate = async (e) => {
+    e.preventDefault();
+    const response = await axios.post(
+      `https://api.shrtco.de/v2/shorten?url=${longUrl}`
+    );
+    console.log(response.data.result);
+    // setLongUrl(response.data.shortUrl);
+  };
+  const getShortUrl = async (e) => {
+    const response = await axios.get(
+      `https://api.shrtco.de/v2/shorten?url=${longUrl}`
+    );
+    console.log(response);
+  };
+  useEffect(() => {
+    (async () => {
+      let temp = await getShortUrl();
+      console.log(temp);
+    })();
+  }, []);
+  const onShortUrlClick = (shortUrl) => {
+    const url = document.getElementsById("copyShort");
+    navigator.clipboard.writeText(url).then(() => {
+      console.log("Copied!");
+    }),
+      (err) => {
+        console.log("Something went wrong", err);
+      };
+  };
   return (
     <>
       <UrlShortnerContainer>
-        <UrlShortnerInput type="text" placeholder="Shorten a link here ..." />
-        <UrlShortnerButton>Shorten It!</UrlShortnerButton>
+        <UrlShortnerInput
+          type="text"
+          placeholder="Shorten a link here ..."
+          value={longUrl}
+          onChange={(e) => setLongUrl(e.target.value)}
+        />
+        <UrlShortnerButton onClick={onCreate}>Shorten It!</UrlShortnerButton>
       </UrlShortnerContainer>
       <UrlShortnerOutputContainer>
         <UrlShortnerInputUrl>
@@ -25,8 +61,12 @@ const UrlShortner = () => {
           </UrlShortnerIUrl>
         </UrlShortnerInputUrl>
         <UrlShortnerOutputUrl>
-          <UrlShortnerOUrl>https://nextjs.org/docs/basic</UrlShortnerOUrl>
-          <UrlShortnerOutputUrlCopy>Copy</UrlShortnerOutputUrlCopy>
+          <UrlShortnerOUrl id="copyShort">
+            https://nextjs.org/docs/basic
+          </UrlShortnerOUrl>
+          <UrlShortnerOutputUrlCopy onClick={() => onShortUrlClick()}>
+            Copy
+          </UrlShortnerOutputUrlCopy>
         </UrlShortnerOutputUrl>
       </UrlShortnerOutputContainer>
     </>
